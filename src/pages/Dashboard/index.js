4cf2ad569e5d16c8b763503df8590d13d48bd703
s1919016 <m.macarie@student.utwente.nl>
2019-11-11 22:22:16 +0200

    DnD working, with bugs regarding this. TO-DO: fix bug and code optimization

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -5,23 +5,72 @@ import LunchFeed from 'widgets/LunchFeed';
 import Weather from 'widgets/Weather';
 import {Draggable, Droppable, DragDropContext} from "react-beautiful-dnd";
 
+
+const getItems = count => {
+    let items = Array.from({length: count}, (v, k) => k).map(k => ({
+        id: `item-${k}`,
+        content: ''
+    }));
+    items[0].content = 'SalesChart';
+    items[1].content = 'Tasks';
+    items[2].content = 'LunchFeed';
+    items[3].content = 'Weather';
+    return items;
+};
+
+
+const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+};
+const components = {
+    saleschart: SalesChart,
+    tasks: Tasks,
+    lunchfeed: LunchFeed,
+    weather: Weather
+};
+
+function Widget(props){
+    const WidgetRenderer = components[props.widgetType.toLowerCase()];
+    console.log(props.widgetType.toLowerCase());
+    return <WidgetRenderer {...props} innerRef={props.innerRef} />;
+}
+
 class Dashboard extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            items: getItems(4),
+        };
+        this.onDragEnd = this.onDragEnd.bind(this);
     }
 
     onDragEnd(result) {
         const {destination, source, draggableId} = result;
+
         console.log(result);
+        console.log(this.state.items);
         if (!destination) {
             return;
         }
+        const items = reorder(
+            this.state.items,
+            result.source.index,
+            result.destination.index
+        );
 
+        this.setState({
+            items,
+        });
     }
 
     render() {
         return (
-            <DragDropContext onDragEnd={this.onDragEnd} direction={"horizontal"}>
+
+            <DragDropContext onDragEnd={this.onDragEnd}>
                 <Droppable droppableId="drop1">
                     {(provided, snapshot) => (
                         <div className="content" ref={provided.innerRef}  {...provided.droppableProps}>
@@ -30,25 +79,30 @@ class Dashboard extends React.Component {
 
                                 <div className="row">
 
-                                    <Draggable key={1} draggableId={1} index={1}>
+                                        <Draggable key={this.state.items[0].id} draggableId={this.state.items[0].id}
+                                                   index={0}>
+
+                                            {(provided, snapshot) => (
+                                                <div className="col-md-6">
+                                                    <Widget widgetType={this.state.items[0].content} innerRef={provided.innerRef}
+                                                                    {...provided.draggableProps}
+                                                                    {...provided.dragHandleProps}/>
+                                                </div>
+
+                                            )}
+
+                                        </Draggable>
+
+
+                                    <Draggable key={this.state.items[1].id} draggableId={this.state.items[1].id}
+                                               index={1}>
 
                                         {(provided, snapshot) => (
                                             <div className="col-md-6">
-                                                <SalesChart innerRef={provided.innerRef}
-                                                            {...provided.draggableProps}
-                                                            {...provided.dragHandleProps}/>
-                                            </div>
-                                        )}
+                                                <Widget widgetType={this.state.items[1].content} innerRef={provided.innerRef}
+                                                        {...provided.draggableProps}
+                                                        {...provided.dragHandleProps}/>
 
-                                    </Draggable>
-
-                                    <Draggable key={2} draggableId={2} index={2}>
-
-                                        {(provided, snapshot) => (
-                                            <div className="col-md-6">
-                                                <Tasks innerRef={provided.innerRef}
-                                                       {...provided.draggableProps}
-                                                       {...provided.dragHandleProps}/>
                                             </div>
 
                                         )}
@@ -60,25 +114,27 @@ class Dashboard extends React.Component {
 
                                 <div className="row">
 
-                                    <Draggable key={3} draggableId={3} index={3}>
+                                    <Draggable key={this.state.items[2].id} draggableId={this.state.items[2].id}
+                                               index={2}>
 
                                         {(provided, snapshot) => (
                                             <div className="col-md-6">
-                                                <LunchFeed innerRef={provided.innerRef}
-                                                           {...provided.draggableProps}
-                                                           {...provided.dragHandleProps}/>
+                                                <Widget widgetType={this.state.items[2].content} innerRef={provided.innerRef}
+                                                        {...provided.draggableProps}
+                                                        {...provided.dragHandleProps}/>
                                             </div>
 
                                         )}
 
                                     </Draggable>
-                                    <Draggable key={4} draggableId={4} index={4}>
+                                    <Draggable key={this.state.items[3].id} draggableId={this.state.items[3].id}
+                                               index={3}>
 
                                         {(provided, snapshot) => (
                                             <div className="col-md-6">
-                                                <Weather innerRef={provided.innerRef}
-                                                         {...provided.draggableProps}
-                                                         {...provided.dragHandleProps}/>
+                                                <Widget widgetType={this.state.items[3].content} innerRef={provided.innerRef}
+                                                        {...provided.draggableProps}
+                                                        {...provided.dragHandleProps}/>
                                             </div>
                                         )}
 
