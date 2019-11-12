225496d4bf53ca84ad78434d1aba47a8c69b772b
s1919016 <m.macarie@student.utwente.nl>
2019-11-12 14:17:02 +0200

    DnD working. TO-DO: code optimization for DnD

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -5,19 +5,46 @@ import LunchFeed from 'widgets/LunchFeed';
 import Weather from 'widgets/Weather';
 import {Draggable, Droppable, DragDropContext} from "react-beautiful-dnd";
 
+const components = {
+    saleschart: SalesChart,
+    tasks: Tasks,
+    lunchfeed: LunchFeed,
+    weather: Weather
+};
 
-const getItems = count => {
-    let items = Array.from({length: count}, (v, k) => k).map(k => ({
-        id: `item-${k}`,
+const getItems = (countRow,offset=0) => {
+
+    let items = Array.from({length: countRow }, (v, k) => k).map(k => ({
+        id: `item-${k+ offset}`,
         content: ''
+
     }));
-    items[0].content = 'SalesChart';
-    items[1].content = 'Tasks';
-    items[2].content = 'LunchFeed';
-    items[3].content = 'Weather';
+    if(offset===0){
+        items[0].content = 'SalesChart';
+        items[1].content = 'Tasks';
+    } else if(offset===2){
+        items[0].content = 'LunchFeed';
+        items[1].content = 'Weather';
+    }
+
+
     return items;
 };
 
+const move = (source, destination, droppableSource, droppableDestination) => {
+    const sourceClone = Array.from(source);
+    const destClone = Array.from(destination);
+    const [removed] = sourceClone.splice(droppableSource.index, 1);
+    const [removed2] = destClone.splice(droppableDestination.index, 1);
+
+    destClone.splice(droppableDestination.index, 0, removed);
+    sourceClone.splice(droppableSource.index,0, removed2);
+    const result = {};
+    result[droppableSource.droppableId] = sourceClone;
+    result[droppableDestination.droppableId] = destClone;
+
+    return result;
+};
 
 const reorder = (list, startIndex, endIndex) => {
     const result = Array.from(list);
@@ -26,126 +53,158 @@ const reorder = (list, startIndex, endIndex) => {
 
     return result;
 };
-const components = {
-    saleschart: SalesChart,
-    tasks: Tasks,
-    lunchfeed: LunchFeed,
-    weather: Weather
-};
 
-function Widget(props){
+
+function Widget(props) {
     const WidgetRenderer = components[props.widgetType.toLowerCase()];
-    console.log(props.widgetType.toLowerCase());
-    return <WidgetRenderer {...props} innerRef={props.innerRef} />;
+    return <WidgetRenderer {...props} innerRef={props.innerRef}/>;
 }
 
 class Dashboard extends React.Component {
+    id2List = {
+        drop1: 'items0',
+        drop2: 'items1'
+    };
+
+    getList = id => this.state[this.id2List[id]];
     constructor(props) {
         super(props);
         this.state = {
-            items: getItems(4),
+            items0: getItems(2,0),
+            items1: getItems(2,2),
         };
+
         this.onDragEnd = this.onDragEnd.bind(this);
     }
 
-    onDragEnd(result) {
-        const {destination, source, draggableId} = result;
-
-        console.log(result);
-        console.log(this.state.items);
+    onDragEnd = result => {
+        const { source, destination } = result;
+       // console.log(result);
+        // dropped outside the list
         if (!destination) {
             return;
         }
-        const items = reorder(
-            this.state.items,
-            result.source.index,
-            result.destination.index
-        );
 
-        this.setState({
-            items,
-        });
-    }
+        if (source.droppableId === destination.droppableId) {
+            const items = reorder(
+                this.getList(source.droppableId),
+                source.index,
+                destination.index
+            );
+
+            let state;
+
+            if (source.droppableId === 'drop2') {
+                state = { items1: items };
+            } else if(source.droppableId === 'drop1'){
+                state = { items0: items };
+            }
+           // console.log(state);
+            this.setState(state);
+        } else {
+            const result = move(
+                this.getList(source.droppableId),
+                this.getList(destination.droppableId),
+                source,
+                destination
+            );
+
+            this.setState({
+                items0: result.drop1,
+                items1: result.drop2
+            });
+        }
+    };
 
     render() {
         return (
 
             <DragDropContext onDragEnd={this.onDragEnd}>
-                <Droppable droppableId="drop1">
-                    {(provided, snapshot) => (
-                        <div className="content" ref={provided.innerRef}  {...provided.droppableProps}>
-                            <div className="container-fluid">
 
+                <div className="content">
+                    <div className="container-fluid">
 
-                                <div className="row">
-
-                                        <Draggable key={this.state.items[0].id} draggableId={this.state.items[0].id}
+                        <Droppable droppableId="drop1">
+                            {(provided, snapshot) => (
+                                <div className="row" ref={provided.innerRef}  {...provided.droppableProps}>
+                                    <div className="col-md-6">
+                                        <Draggable key={this.state.items0[0].id} draggableId={this.state.items0[0].id}
                                                    index={0}>
 
                                             {(provided, snapshot) => (
-                                                <div className="col-md-6">
-                                                    <Widget widgetType={this.state.items[0].content} innerRef={provided.innerRef}
-                                                                    {...provided.draggableProps}
-                                                                    {...provided.dragHandleProps}/>
-                                                </div>
+
+                                                <Widget widgetType={this.state.items0[0].content}
+                                                        innerRef={provided.innerRef}
+                                                        {...provided.draggableProps}
+                                                        {...provided.dragHandleProps}/>
+
 
                                             )}
 
                                         </Draggable>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <Draggable key={this.state.items0[1].id} draggableId={this.state.items0[1].id}
+                                                   index={1}>
 
+                                            {(provided, snapshot) => (
 
-                                    <Draggable key={this.state.items[1].id} draggableId={this.state.items[1].id}
-                                               index={1}>
-
-                                        {(provided, snapshot) => (
-                                            <div className="col-md-6">
-                                                <Widget widgetType={this.state.items[1].content} innerRef={provided.innerRef}
+                                                <Widget widgetType={this.state.items0[1].content}
+                                                        innerRef={provided.innerRef}
                                                         {...provided.draggableProps}
                                                         {...provided.dragHandleProps}/>
 
-                                            </div>
 
-                                        )}
+                                            )}
 
-                                    </Draggable>
-
-
+                                        </Draggable>
+                                    </div>
+                                    {provided.placeholder}
                                 </div>
+                            )}
+                        </Droppable>
+                        <Droppable droppableId="drop2">
+                            {(provided, snapshot) => (
+                                <div className="row" ref={provided.innerRef}  {...provided.droppableProps}>
+                                    <div className="col-md-6">
+                                        <Draggable key={this.state.items1[0].id} draggableId={this.state.items1[0].id}
+                                                   index={0}>
 
-                                <div className="row">
+                                            {(provided, snapshot) => (
 
-                                    <Draggable key={this.state.items[2].id} draggableId={this.state.items[2].id}
-                                               index={2}>
-
-                                        {(provided, snapshot) => (
-                                            <div className="col-md-6">
-                                                <Widget widgetType={this.state.items[2].content} innerRef={provided.innerRef}
+                                                <Widget widgetType={this.state.items1[0].content}
+                                                        innerRef={provided.innerRef}
                                                         {...provided.draggableProps}
                                                         {...provided.dragHandleProps}/>
-                                            </div>
 
-                                        )}
 
-                                    </Draggable>
-                                    <Draggable key={this.state.items[3].id} draggableId={this.state.items[3].id}
-                                               index={3}>
+                                            )}
 
-                                        {(provided, snapshot) => (
-                                            <div className="col-md-6">
-                                                <Widget widgetType={this.state.items[3].content} innerRef={provided.innerRef}
+                                        </Draggable>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <Draggable key={this.state.items1[1].id} draggableId={this.state.items1[1].id}
+                                                   index={1}>
+
+                                            {(provided, snapshot) => (
+
+                                                <Widget widgetType={this.state.items1[1].content}
+                                                        innerRef={provided.innerRef}
                                                         {...provided.draggableProps}
                                                         {...provided.dragHandleProps}/>
-                                            </div>
-                                        )}
 
-                                    </Draggable>
+                                            )}
 
+                                        </Draggable>
+                                    </div>
+                                    {provided.placeholder}
                                 </div>
-                            </div>
-                            {provided.placeholder}
-                        </div>
-                    )}
-                </Droppable>
+                            )}
+                        </Droppable>
+                    </div>
+
+                </div>
+
             </DragDropContext>
         )
     }
