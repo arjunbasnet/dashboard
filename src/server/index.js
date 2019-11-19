7bb9c529d9cecbae02907d5ae6e211b27e823654
s1919016 <m.macarie@student.utwente.nl>
2019-11-19 21:23:42 +0200

    Database API for models done

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const os = require('os');
+
 const usersRouter = require('./routes/users');
+const widgetsRouter = require('./routes/widgets');
+const widgetConfigRouter = require('./routes/widgetconfig');
+const dashboardRouter = require('./routes/dashboard');
+
+
 const mongoose = require("mongoose");
 const dbPath = "mongodb://localhost/test";
 var path = require('path');
-
-
-
 
 mongoose.connect(dbPath, {
     useNewUrlParser: true,
@@ -22,6 +25,9 @@ db.once("open", () => {
 app.use(express.json());
 app.use(express.static('dist'));
 app.use('/api/users', usersRouter);
+app.use('/api/widgets', widgetsRouter);
+app.use('/api/widgetConfig', widgetConfigRouter);
+app.use('/api/dashboard', dashboardRouter);
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
 app.get('*', function(req, res) {
--- src/server/routes/dashboard.js
+++ src/server/routes/dashboard.js
@@ -1,40 +1,38 @@
 const express = require('express');
 const router = express.Router();
-let User = require( "../models/User");
-//get all users
+let DashboardConfig = require( "../models/DashboardConfig");
+
 router.get('/', async (req, res) => {
-    let users = await User.find();
-    return res.status(200).send(users);
+    let dash = await DashboardConfig.find();
+    return res.status(200).send(dash);
 });
 
-//get one user
 router.get('/:id', async (req, res) => {
     const {id} = req.params;
-    let user = await User.findById(id);
+    let dash = await DashboardConfig.findById(id);
     return res.status(200).send({
         error: false,
-        user
+        dash
     });
 });
 
-//create user
+
 router.post('/', async (req, res) => {
-    let user = await User.create(req.body);
+    let dash = await DashboardConfig.create(req.body);
     return res.status(201).send({
         error: false,
-        user
+        dash
     })
 });
 
-//update user
 router.put(`/:id`, async (req, res) => {
     const {id} = req.params;
 
-    let user = await User.findByIdAndUpdate(id, req.body);
+    let dash = await DashboardConfig.findByIdAndUpdate(id, req.body);
 
     return res.status(202).send({
         error: false,
-        user
+        dash
     })
 
 });
@@ -42,11 +40,11 @@ router.put(`/:id`, async (req, res) => {
 router.delete(`/:id`, async (req, res) => {
     const {id} = req.params;
 
-    let user = await User.findByIdAndDelete(id);
+    let dash = await DashboardConfig.findByIdAndDelete(id);
 
     return res.status(202).send({
         error: false,
-        user
+        dash
     })
 
 });
